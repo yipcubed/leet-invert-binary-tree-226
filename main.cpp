b6f34544f62944b817e8faece356c22becbe1071
Yip Cubed <yipcubed@gmail.com>
2019-06-14 03:15:26 +0000

    starting

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+*/
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+      if (root) {
+        TreeNode* newLeft = invertTree(root->right);
+        root->right = invertTree(root->left);
+        root->left = newLeft;
+      }
+      return root;
+    }
+};
+
+
+void print(TreeNode* root) {
+  if (root) {
+    print(root->left);
+    cout << root->val << " ";
+    print(root->right);
+  }
+}
+/*
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+*/
+
+
+int main() {
+  Solution S;
+
+  TreeNode* t1 = new TreeNode(4);
+  t1->left = new TreeNode(2);
+  t1->left->left = new TreeNode(1);
+  t1->left->right = new TreeNode(3);
+
+  t1->right = new TreeNode(7);
+  t1->right->left = new TreeNode(6);
+  t1->right->right = new TreeNode(9);
+
+  print(t1);
+  cout << endl;
+
+  print(S.invertTree(t1));
+  cout << endl;
+
+  // delete stuff
+  std::cout << "Done\n";
+}
